c5c48f2d3ef113f67fafd8e2e0462c7b8bd9e569
DESKTOP-MDJI06Q\mitra <mitrasca.rares@gmail.com>
2021-09-26 17:10:04 +0300

    Added fields to location and parameter serialization

--- weather/api/serializers.py
+++ weather/api/serializers.py
@@ -4,17 +4,19 @@ from .models import Location, Parameter
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
-        fields = ['id', 'description', 'owner', 'aggregation']
+        fields = ['id', 'description', 'owner', 'lat', 'lon', 'aggregation']
         extra_kwargs = {
             'id': {'read_only': True},
             'owner': {'write_only': True},
+            'aggregation': {'read_only': True},
         }
 
 class ParameterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Parameter
-        fields = ['id', 'name', 'values', 'location']
+        fields = ['id', 'name', 'unit', 'api_id', 'values', 'location']
         extra_kwargs = {
             'id': {'read_only': True},
             'location': {'write_only': True},
+            'values': {'read_only': True},
         }
